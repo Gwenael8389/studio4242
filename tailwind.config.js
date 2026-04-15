2baf7eafdc361f20e2a4eff0faaa500c9658d11f
Gwenael8389 <gwenael.drouet@alumni.enac.fr>
2026-04-15 16:33:49 +0200

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#eff6ff',
+          100: '#dbeafe',
+          200: '#bfdbfe',
+          600: '#2563eb',
+          700: '#1d4ed8',
+          900: '#1e3a8a',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
